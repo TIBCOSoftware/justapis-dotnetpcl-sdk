3618c70bb4ae37e9079d7d3e921a621eec09a6fd
Khem Veasna <kveasna@gmail.com>
2016-03-10 16:58:08 -0500

    Add ios project

---
+++ APGW_IOS/APGateway.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Net.Security;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using APGW;
+using System.Threading.Tasks;
+using Common;
+
+namespace APGW_IOS
+{
+    public class APGateway: APGW.APGateway
+    {
+        public APGateway ()
+        {   
+            RestClient = new APHttpWebRequestClient ();
+        }
+
+        public APGateway UsePinning(bool state) {            
+            base.UsePinning (state);
+
+            if (state == true) {
+                ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(CertPolicy.ValidateServerCertificate);
+                ServicePointManager.MaxServicePointIdleTime = 0;
+            }
+            return this;
+        }
+    }
+}
+
---
+++ TEST_APGW_IOS_UNIT/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TEST_APGW_IOS_UNIT
+{
+    public class MyClass
+    {
+        public MyClass ()
+        {
+        }
+    }
+}
+
