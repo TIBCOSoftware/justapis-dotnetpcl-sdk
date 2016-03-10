fdbd23d938e5540f2539706e0c430be12687591f
Khem Veasna <kveasna@gmail.com>
2016-03-10 16:45:24 -0500

    Added shared Commons project

---
+++ Common/APHttpWebRequestClient.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using APGW;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Common
+{
+    public class APHttpWebRequestClient: IAPRestClient
+    {
+        /// <summary>
+        /// Hold onto the cookie container
+        /// </summary>
+        private CookieContainer cookieContainer = new CookieContainer();
+
+        public APHttpWebRequestClient ()
+        {
+        }
+
+        private HttpWebRequest CreateClient(string url) {
+            HttpWebRequest client = WebRequest.CreateHttp (url);
+
+            client.CookieContainer = cookieContainer;
+
+            return client;
+        }
+
+        public TransformedResponse<WebResponse> ReadResponse() {
+            return null;
+        }
+
+        private void PinCerts(HttpWebRequest client) {
+            LogHelper.Log ("CORE: Pinning certs...");
+
+            client.ClientCertificates.Clear ();
+
+            foreach (var values in CertManager.GetCerts()) {
+                X509Certificate cert = new X509Certificate (values.Value);
+              
+                client.ClientCertificates.Add (cert);
+            }
+        }
+
+        public async Task<IResponse> ExecuteRequest<T>(RequestContext<T> request) {
+            LogHelper.Log ("Executing request with HttpWebRequest: " + request.Url);
+
+            var client = CreateClient (request.Url);
+
+            if (request.Gateway != null && request.Gateway.ShouldUsePinning ()) {
+                PinCerts (client);
+            }
+
+            if (request.Method == HTTPMethod.POST || request.Method == HTTPMethod.PUT) {
+                // Send a post
+                client.Method = "POST";
+
+                LogHelper.Log ("CORE: finished getting response");
+
+                WebResponse response = await client.GetResponseAsync ();
+
+                return new HttpWebRequestResponse (response);
+            } else { 
+                // Send a get
+                client.Method = "GET";
+
+                LogHelper.Log ("CORE: finished getting response");
+
+                WebResponse response = await client.GetResponseAsync ();
+
+                return new HttpWebRequestResponse (response);
+            }   
+        }
+            
+    }
+}
+
+
---
+++ Common/HttpWebRequestResponse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using APGW;
+using System.Net;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Common
+{
+    public class HttpWebRequestResponse: IResponse
+    {
+        private WebResponse response;
+        private String requestUri;
+
+        public HttpWebRequestResponse() {}
+               
+        public HttpWebRequestResponse (WebResponse response)
+        {
+            this.response = response;
+        }
+
+        public string RequestUri() {
+            return response.ResponseUri.ToString ();
+        }
+
+        public async Task<string> ReadResponseBodyAsString() {
+            using (Stream responseStream = response.GetResponseStream())
+            using (StreamReader sr = new StreamReader(responseStream))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        public Dictionary<string, List<string>> Headers() {
+            return null;
+        }
+
+        public CacheControlOptions CacheControl() {
+            return new CacheControlOptions (false);
+        }
+    }
+}
+
