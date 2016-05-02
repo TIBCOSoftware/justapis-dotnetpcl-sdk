20089dfa8363eec364cca25e17730c735f222d90
Khem Veasna <kveasna@gmail.com>
2016-05-02 16:35:35 -0400

    [#118754281] Fix issue with building urls

--- TEST_APGW_CORE/UnitTestUtilities.cs
+++ TEST_APGW_CORE/UnitTestUtilities.cs
@@ -28,13 +28,30 @@ namespace TEST_APGW_CORE
         [Test]
         public void Test_UpdateUrl() {
             string baseUrl = "http://localhost/api/v1";
-
             string newUrl = "/foo";
-
             string resolvedUrl = Utilities.UpdateUrl (baseUrl, newUrl);
 
             Assert.AreEqual ("http://localhost/api/v1/foo", resolvedUrl);
+                       
+            baseUrl = "http://localhost/api/v1";
+            newUrl = "foo";
+            resolvedUrl = Utilities.UpdateUrl (baseUrl, newUrl);
 
+            Assert.AreEqual ("http://localhost/api/v1/foo", resolvedUrl);
+
+
+            baseUrl = "http://localhost/api/v1";
+            newUrl = "http://localhost/api/v1/foobar";
+            resolvedUrl = Utilities.UpdateUrl (baseUrl, newUrl);
+
+            Assert.AreEqual ("http://localhost/api/v1/foobar", resolvedUrl);
+
+            baseUrl = "http://foo.lvh.me:5000";
+            newUrl = "/bar";
+            resolvedUrl = Utilities.UpdateUrl (baseUrl, newUrl);
+
+            Assert.AreEqual ("http://foo.lvh.me:5000/bar", resolvedUrl);
+  
         }
     }
 }
